a4c8fc7853af07170a602da45d68cfc7e619d8b7
Henrique Matos <2000rikematos@gmail.com>
2026-06-26 16:54:09 +0100

    added readme

---
+++ Backend/schema.sql
@@ -0,0 +1,42 @@
+CREATE TABLE IF NOT EXISTS public.workplaces (
+    id uuid NOT NULL UNIQUE,
+    manager_password character varying NOT NULL,
+    operator_password character varying NOT NULL,
+    company_name character varying NOT NULL,
+    login_name character varying NOT NULL UNIQUE,
+    CONSTRAINT workplaces_pkey PRIMARY KEY (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS public.operators (
+    id uuid NOT NULL,
+    first_name character varying NOT NULL,
+    last_name character varying NOT NULL,
+    phone character varying NOT NULL,
+    workplace_id uuid NOT NULL,
+    internal_number integer NOT NULL,
+    CONSTRAINT operators_pkey PRIMARY KEY (id),
+    CONSTRAINT operators_workplace_id_fkey FOREIGN KEY (workplace_id) REFERENCES public.workplaces(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS public.tasks (
+    id uuid NOT NULL,
+    task character varying NOT NULL,
+    workplace_id uuid NOT NULL,
+    CONSTRAINT tasks_pkey PRIMARY KEY (id),
+    CONSTRAINT tasks_workplace_id_fkey FOREIGN KEY (workplace_id) REFERENCES public.workplaces(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS public.active_tasks (
+    id uuid NOT NULL,
+    operator_id uuid NOT NULL,
+    task_id uuid NOT NULL,
+    time_start bigint NOT NULL,
+    workplace_id uuid NOT NULL,
+    CONSTRAINT active_tasks_pkey PRIMARY KEY (id),
+    CONSTRAINT active_tasks_operator_id_fkey FOREIGN KEY (operator_id) REFERENCES public.operators(id) ON DELETE CASCADE,
+    CONSTRAINT active_tasks_task_id_fkey FOREIGN KEY (task_id) REFERENCES public.tasks(id) ON DELETE CASCADE,
+    CONSTRAINT active_tasks_workplace_id_fkey FOREIGN KEY (workplace_id) REFERENCES public.workplaces(id) ON DELETE CASCADE
+);
---
+++ README.md
@@ -0,0 +1,34 @@
+# 🛠️ Workplace
+
+Workplace is an app for managing a company's staff 
+
+---
+
+## 🚀 Technologies Used 
+
+### Frontend
+- **React** (with TypeScript)
+- **Vite** (build e development)
+- **CSS3** (styling)
+
+### Backend
+- **Node.js** & **Express** (with TypeScript)
+- **PostgreSQL** (hosted via supabase)
+
+---
+
+## 📦 How to install and run locally
+
+# 1. Clone the repository
+
+# 2 Go to /Backend and run npm install
+
+# 3 Go to /Frontend and run npm install
+
+# 4 Go to to both directories and change the name of the file .env.example to .env in both directories and add your variables there
+
+# 5 Go to /Backend/schema.sql and copy the queries to your SQL editor
+
+# 6 Go to /Backend and run npm run build and then npm start
+
+# 7 Go to /Frontend and run npm run build and then npm run preview
